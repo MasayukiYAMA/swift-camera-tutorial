7174dc0d7b7677845e531c5b7ed2253afafcbf43
Masayuki YAMADA <masaofyamad@gmail.com>
2019-06-12 16:31:55 +0900

    Initial Commit

---
+++ Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Masayuki YAMADA on 2019/06/12.
+//  Copyright © 2019 Masayuki YAMADA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
